4a5467b5e6ef8519a25ee1c581b45284587334bb
Коля <@DESKTOP-3O0178S>
2022-11-22 21:49:27 +0200

    text

---
+++ Bassic CPP/Source.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Name" << "\t\t:\"The war and the pease\"" << endl;
+	cout << "Avtor" << "\t\t:L.N. Tolstoj" << endl;
+	cout << "Izdatelsvo" << "\t:Piter" << endl;
+	cout << "Pages" << "\t\t: 500" << endl << endl << endl;
+	cout << "\t«Kraj lukomor’a dub zelenyj," << endl;
+	cout << "\tI zolotyj lancyuh na nim:" << endl;
+	cout << "\tShodnya, shonochi kit uchenyj" << endl;
+	cout << "\tNa lancyuhu kruzhl’aje tim”" << endl << endl << endl << endl;
+	cout << "\n" << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 << endl;
+	cout << "\n" << (char)186 << "\tPora Roku            " << (char)186 << endl;
+	cout << "\n" << (char)204 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)185 << endl;
+	cout << "\n" << (char)186 << "zuma  " << (char)186 << "Vesna  " << (char)186 << "leto  " << (char)186 << "osin  " << (char)186 << endl;
+	cout << "\n" << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)188 << endl;
+	cin.get();
+
+	return 0;
+}
